9421c5b04ac32d18ebf4eaa1b1d7bafb9b689454
alexandresoueu <alexandresoueu@gmail.com>
2020-03-06 16:01:27 -0300

    create first test

---
+++ src/components/QuestionList.spec.js
@@ -0,0 +1,24 @@
+describe("The question list", () => {
+    beforeEach(() => {
+        console.log("pinguiLee")
+    })
+    beforeAll(() => {
+        console.log("bulbassauro")
+    })
+    afterEach(() => {
+        console.log("Cachorro ALADO")
+    })
+    afterAll(() => {
+        console.log("Dagron Latindo")
+    })
+    
+    it("Should display a list of items", () => {
+        expect(40 + 2).toEqual(42)
+    })
+    it("Should display a list of items", () => {
+        expect(40 + 2).toEqual(42)
+    })
+    it("Should display a list of items", () => {
+        expect(40 + 2).toEqual(42)
+    })
+})
