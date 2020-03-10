b4e252e2e25780d0a26b93d54d0ca73a00fcea46
alexandresoueu <alexandresoueu@gmail.com>
2020-03-10 14:09:49 -0300

    teste working

--- src/sagas/fetch-question-saga.spec.js
+++ src/sagas/fetch-question-saga.spec.js
@@ -1,7 +1,7 @@
 import { handleFetchQuestion }  from './fetch-question-saga'
 import fetch from 'isomorphic-fetch'
 
-describe.skip("Fetch questions saga",()=>{
+describe("Fetch questions saga",()=>{
     beforeAll(()=>{
         fetch.__setValue([{question_id:42}])
     })
@@ -9,7 +9,6 @@ describe.skip("Fetch questions saga",()=>{
     it("should get the questions from the correct endpoint in response to the appropriate action",async ()=>{
         const gen = handleFetchQuestion({question_id:42})
         const { value } = await gen.next()
-        console.log("MINHAU....", value)
         
         expect(value).toEqual([{question_id:42}])
         expect(fetch).toHaveBeenCalledWith(`/api/questions/42`)
